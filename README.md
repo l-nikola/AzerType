852b3a00cbc8e91778aa36ebb5a8690b8231695f
l-nikola <legay.nikola.pro@gmail.com>
2024-05-29 20:51:43 +0200

    Start Back

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # AzerType
+
+L’objectif de cette application est d’apprendre à taper plus vite au clavier, en tapant le plus rapidement possible et dans un temps imparti des mots proposés par l’application.
+
+Étape 1 : L’application propose un mot.
+
+Étape 2 : L’utilisateur tape ce mot au clavier.
+
+Étape 3 : Si le mot de l’utilisateur est exactement le même que le mot de l’application, alors on ajoute un point au score.
+
+Étape 4 : On passe au mot suivant.
+
+Étape 5 : On recommence à l’étape 1, jusqu’à ce que le temps soit écoulé.
---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+function afficherResultat(score, nbMotsProposes) {
+  console.log("Votre score est de " + score + " sur " + nbMotsProposes);
+}
+
+function choisirPhrasesOuMots() {
+  let choix = prompt(
+    "Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?"
+  );
+
+  while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt(
+      "Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?"
+    );
+  }
+
+  return choix;
+}
+
+function lancerBoucleDeJeu(listeProposition) {
+  let score = 0;
+  for (let i = 0; i < listeProposition.length; i++) {
+    let motUtilisateur = prompt("Entrez le mot : " + listeProposition[i]);
+    if (motUtilisateur === listeProposition[i]) {
+      score++;
+    }
+  }
+
+  return score;
+}
+
+function lancerJeu() {
+  let choix = choisirPhrasesOuMots();
+  let score = 0;
+  let nbMotsProposes = 0;
+
+  if (choix === "mots") {
+    score = lancerBoucleDeJeu(listeMots);
+    nbMotsProposes = listeMots.length;
+  } else {
+    score = lancerBoucleDeJeu(listePhrases);
+    nbMotsProposes = listePhrases.length;
+  }
+
+  afficherResultat(score, nbMotsProposes);
+}
