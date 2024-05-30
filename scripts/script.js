7028a105811535790ad63920394c0a12e2d6678e
l-nikola <legay.nikola.pro@gmail.com>
2024-05-30 16:18:47 +0200

    Functional application

--- scripts/script.js
+++ scripts/script.js
@@ -9,29 +9,100 @@ function afficherProposition(proposition) {
   zoneProposition.innerText = proposition;
 }
 
+function afficherEmail(nom, email, score) {
+  let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`;
+  location.href = mailto;
+}
+
+function validerNom(nom) {
+  if (nom.length < 2) {
+    throw new Error("Le nom est trop court.");
+  }
+}
+
+function validerEmail(email) {
+  let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (!emailRegExp.test(email)) {
+    throw new Error("L'adresse email n'est pas valide.");
+  }
+}
+
+function afficherMessageErreur(message) {
+  let spanErreurMessage = document.getElementById("erreurMessage");
+
+  if (!spanErreurMessage) {
+    let popup = document.querySelector(".popup");
+
+    spanErreurMessage = document.createElement("span");
+    spanErreurMessage.id = "erreurMessage";
+    popup.append(spanErreurMessage);
+  }
+
+  spanErreurMessage.innerText = message;
+}
+
+function gererFormulaire(scoreEmail) {
+  try {
+    let baliseNom = document.getElementById("nom");
+    let nom = baliseNom.value;
+    validerNom(nom);
+
+    let baliseEmail = document.getElementById("email");
+    let email = baliseEmail.value;
+    validerEmail(email);
+    afficherMessageErreur("");
+    afficherEmail(nom, email, scoreEmail);
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message);
+  }
+}
+
 function lancerJeu() {
+  initAddEventListenerPopup();
   let score = 0;
   let i = 0;
+  let listeProposition = listeMots;
 
   let inputEcriture = document.getElementById("inputEcriture");
   let btnValiderMot = document.getElementById("btnValiderMot");
 
-  afficherProposition(listeMots[i]);
+  afficherProposition(listeProposition[i]);
 
   btnValiderMot.addEventListener("click", () => {
-    if (inputEcriture.value === listeMots[i]) {
+    if (inputEcriture.value === listeProposition[i]) {
       score++;
     }
     i++;
     afficherResultat(score, i);
     inputEcriture.value = "";
 
-    if (listeMots[i] === undefined) {
+    if (listeProposition[i] === undefined) {
       afficherProposition("Le jeu est fini");
       btnValiderMot.disabled = true;
     } else {
-      afficherProposition(listeMots[i]);
+      afficherProposition(listeProposition[i]);
     }
   });
+
+  let listeBtnRadio = document.querySelectorAll(".optionSource input");
+
+  for (index = 0; index < listeBtnRadio.length; index++) {
+    listeBtnRadio[index].addEventListener("change", (event) => {
+      if (event.target.value === "1") {
+        listeProposition = listeMots;
+      } else {
+        listeProposition = listePhrases;
+      }
+      afficherProposition(listeProposition[i]);
+    });
+  }
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let scoreEmail = `${score} / ${i}`;
+    gererFormulaire(scoreEmail);
+  });
+
   afficherResultat(score, i);
 }
